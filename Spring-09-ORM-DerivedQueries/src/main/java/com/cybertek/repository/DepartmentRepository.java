c33f4b1299a85980ac1d66d66ccfa47bd4e6f304
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-12-22 20:12:30 -0500

    new

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
@@ -1,4 +1,23 @@
 package com.cybertek.repository;
 
-public interface DepartmentRepository {
+import com.cybertek.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, String> {
+
+List<Department>findByDepartment(String department);
+
+List<Department>findByDivision(String division);
+
+List<Department>findByDivisionIs(String division);
+
+List<Department>findByDivisionEquals(String division);
+
+
+
+
 }
