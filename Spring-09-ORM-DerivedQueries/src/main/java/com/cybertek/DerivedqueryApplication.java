377c05a56da144982ba42b8b4437fe5f346c681a
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-12-22 21:28:32 -0500

    last for today

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
@@ -1,5 +1,7 @@
 package com.cybertek;
 
+import com.cybertek.repository.DepartmentRepository;
+import com.cybertek.repository.EmployeeRepository;
 import com.cybertek.repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -13,13 +15,17 @@ public class DerivedqueryApplication {
 
     @Autowired
     RegionRepository regionRepository;
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DerivedqueryApplication.class, args);
     }
 
     @PostConstruct
-    public void testRegions() {
+    public void testRegions(){
 
         System.out.println("---------Regions start------------");
 
@@ -30,17 +36,34 @@ public class DerivedqueryApplication {
         System.out.println("findByCountryContainingOrderByCountry: " + regionRepository.findByCountryContainingOrderByCountry("Asia"));
         System.out.println("findTopBy2ByCountry: " + regionRepository.findTop2ByCountry("Canada"));
 
-
         System.out.println("---------Regions End------------");
 
     }
 
     @PostConstruct
-    public void testEmployee() {
+    public void testDepartments(){
 
-        System.out.println("---------Regions start------------");
+        System.out.println("---------Department start------------");
 
-        System.out.println(g);
-        System.out.println("---------Regions End------------");
+        System.out.println("findByDepartment: " + departmentRepository.findByDepartment("Toys"));
+        System.out.println("findByDivision: " + departmentRepository.findByDivision("Outdoors"));
+        System.out.println("findByDivisionEndingWith: " + departmentRepository.findByDivisionEndingWith("ics"));
+        System.out.println("findDistinctTop3ByDivisionContains: " + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+        System.out.println("---------Department End------------");
+
     }
+
+    @PostConstruct
+    public void testEmployees(){
+
+        System.out.println("---------Employee start------------");
+
+        System.out.println(employeeRepository.findByEmailIsNull());
+
+
+        System.out.println("---------Employee End------------");
+
+    }
+
 }
