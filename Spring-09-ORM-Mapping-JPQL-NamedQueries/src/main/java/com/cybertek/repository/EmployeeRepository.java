d9fb5a93d596c3ca5b3c8b92ada4fec4c680bd7c
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-12-29 22:00:03 -0500

    new queries with NULL

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -3,8 +3,11 @@ package com.cybertek.repository;
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -24,6 +27,50 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query("SELECT e FROM Employee  e WHERE e.email = ?1 and e.salary = ?2")
     Employee getEmployeeByEmailAndSalary(String email, int salary);
 
+    //single named parameter
+
+    @Query("SELECT e FROM Employee e WHERE e.salary = :salary")
+    Employee getEmployeeBySalary(@Param("salary") int salary);
+
+    //multiple named parameters
+    @Query("SELECT e FROM Employee e WHERE e.firstName = :name or e.salary = :salary")
+    List<Employee>getEmployeeByFirstNameOrSalary(@Param("name")String name, @Param("salary") int salary);
+
+    // Not Equal
+    @Query("SELECT e FROM Employee e WHERE e.salary <> ?1")
+    List<Employee>getEmployeeBySalaryNotEqual(int salary);
+
+    //Like / Contains / Start With / Ends With
+    @Query("SELECT e FROM Employee  e WHERE e.firstName LIKE ?1")
+    List<Employee>getEmployeeByFirstNameLike(String pattern);
+
+    //Less Than
+    @Query("SELECT e FROM Employee e WHERE e.salary < ?1")
+    List<Employee> getEmployeeBySalaryLessThan(int salary);
+
+    //Greater Than
+    @Query("SELECT e FROM Employee  e WHERE e.salary > ?1")
+    List<Employee>getEmployeeBySalaryGreaterThan(int salary);
+
+    //Between
+    @Query("SELECT e FROM Employee e WHERE e.salary BETWEEN ?1 AND ?2")
+    List<Employee>getEmployeeBySalaryBetween(int salary1, int salary2);
+
+    //Before
+    @Query("SELECT e FROM Employee e WHERE e.hireDate > ?1")
+    List<Employee>getEmployeeByHireDateBefore(LocalDate date);
+
+    //After
+    @Query("SELECT e FROM Employee e WHERE e.hireDate < ?1")
+    List<Employee>getEmployeeByHireDateAfter(LocalDate date);
+
+    //Null
+    @Query("SELECT e FROM Employee e WHERE e.email IS NULL")
+    List<Employee>getEmployeeByEmailIsNull();
+
+    //Not Null
+    @Query("SELECT e FROM Employee e WHERE e.email IS NOT NULL")
+    List<Employee>getEmployeeByEmailIsNotNull();
 
 
 }
