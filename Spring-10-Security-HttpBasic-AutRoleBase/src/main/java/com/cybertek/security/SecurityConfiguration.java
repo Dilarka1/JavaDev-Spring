3762cad1237cf7449c9608be4083972e62b95329
Dilara Khafizov <dilara_khafizova@gmail.com>
2021-01-25 21:58:44 -0500

    new stuff

--- Spring-10-Security-HttpBasic-AutRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ Spring-10-Security-HttpBasic-AutRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -12,17 +12,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()    //request should be authorized
+                .authorizeRequests() //request should be authorized
                 .antMatchers("index.html").permitAll()
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("management/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
                 .and()
-                .httpBasic();   //perform basic http authentication
-
+                .httpBasic(); //perform basic http authentication
     }
 
     @Override
@@ -33,11 +33,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("ozzy").password(passwordEncoder().encode("ozzy123")).roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MAMAGER");
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
+
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 }
