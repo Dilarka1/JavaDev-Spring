abb2b948d232372bcb5e3c9fa323e12aacb00eec
Dilara Khafizov <dilara_khafizova@gmail.com>
2021-01-27 22:30:46 -0500

    login

--- Spring-10-Security-FormAuth/src/main/java/com/cybertek/controller/ManagementController.java
+++ Spring-10-Security-FormAuth/src/main/java/com/cybertek/controller/ManagementController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping("/management")
 public class ManagementController {
@@ -12,6 +11,5 @@ public class ManagementController {
     @GetMapping("/index")
     public String index() {
         return "management/index";
-
     }
 }
