d778f76fc33ca1492c8c48e9a3c30079f7c89867
Dilara Khafizov <dilara_khafizova@gmail.com>
2021-02-05 22:15:54 -0500

    Web Client

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,16 +1,19 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Address;
 import com.cybertek.model.ResponseWrapper;
 import com.cybertek.model.Teacher;
+import com.cybertek.repository.AddressRepository;
 import com.cybertek.repository.ParentRepository;
 import com.cybertek.repository.StudentRepository;
 import com.cybertek.repository.TeacherRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiController {
@@ -19,49 +22,53 @@ public class ApiController {
     private TeacherRepository teacherRepository;
     private StudentRepository studentRepository;
     private ParentRepository parentRepository;
-  //  private AddressRepository addressRepository;
+    private AddressRepository addressRepository;
 
-    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository, ParentRepository parentRepository){ //, AddressRepository addressRepository) {
+    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository, ParentRepository parentRepository, AddressRepository addressRepository) {
         this.teacherRepository = teacherRepository;
         this.studentRepository = studentRepository;
         this.parentRepository = parentRepository;
-     //   this.addressRepository = addressRepository;
+        this.addressRepository = addressRepository;
     }
 
     @GetMapping("/teachers")
-    public List<Teacher> redAllTeachers() {
+    public List<Teacher> redAllTeachers(){
         return teacherRepository.findAll();
     }
 
     @GetMapping("/students")
-    public ResponseEntity<ResponseWrapper> readAllStudents() {
+    public ResponseEntity<ResponseWrapper> readAllStudents(){
 
         return ResponseEntity
-                .ok(new ResponseWrapper("students are successfully retrieved", studentRepository.findAll()));
+                .ok(new ResponseWrapper("students are successfully retrieved",studentRepository.findAll()));
     }
 
     @GetMapping("/parents")
-    public ResponseEntity<ResponseWrapper> readAllParents() {
+    public ResponseEntity<ResponseWrapper> readAllParents(){
 
-        ResponseWrapper responseWrapper = new ResponseWrapper(true, "Parents are successfully retrieved",
+        ResponseWrapper responseWrapper = new ResponseWrapper(true,"Parents are successfully retrieved",
                 HttpStatus.ACCEPTED.value(),
                 parentRepository.findAll());
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseWrapper);
     }
 
-//    @PutMapping("/address/{id}")
-//    public Address updateAddress(@PathVariable("id") long id, @RequestBody Address address) throws Exception {
-//
-//        Optional<Address> foundAddress = addressRepository.findById(id);
-//
-//        if (!foundAddress.isPresent()) {
-//            throw new Exception("Address does not exists!");
-//        }
-//
-//        address.setCurrentTemperature(new Address().consumeTemp(address.getCity()));
-//        address.setId(foundAddress.get().getId());
-//
-//        return addressRepository.save(address);
-//    }
+    @PutMapping("/address/{id}")
+    public Address updateAddress(@PathVariable("id") long id, @RequestBody Address address) throws Exception {
+
+        Optional<Address> foundAddress = addressRepository.findById(id);
+
+        if(!foundAddress.isPresent()){
+            throw new Exception("Address does not exists!");
+        }
+
+        address.setCurrentTemperature(new Address().consumeTemp(address.getCity()));
+        address.setId(foundAddress.get().getId());
+
+        return addressRepository.save(address);
+
+
+    }
+
+
 }
--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Address.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/model/Address.java
@@ -2,20 +2,21 @@ package com.cybertek.model;
 
 import com.cybertek.enums.AddressType;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.web.client.RestTemplate;
+
 import javax.persistence.*;
+import java.util.Map;
 
 @Entity
 @Table(name = "address")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "teacher"}, ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Address extends BaseEntity {
@@ -36,7 +37,7 @@ public class Address extends BaseEntity {
     private Student student;
 
     @OneToOne(mappedBy = "address")
-    @JsonBackReference
+    @JsonIgnore
     private Parent parent;
 
     @OneToOne(mappedBy = "address")
@@ -44,11 +45,24 @@ public class Address extends BaseEntity {
 
     private Integer currentTemperature;
 
-    private Integer getCurrentTemperature() {
-        return consumeTemperature(this.city);
-    }
+//    private Integer getCurrentTemperature(){
+//        return consumeTemp(this.city);
+//    }
 
-    public Integer consumeTemperature(String city) {
-        return 5;
+    public Integer consumeTemp(String city) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String BASE_URL = "http://api.weatherstack.com/current?access_key=02a009b8e3922c395677a1e85406aca6&query=";
+
+        String uri = BASE_URL + city;
+
+        Object currentWeather = restTemplate.getForObject(uri, Object.class);
+
+        Map<String, Object> getWeather = (Map<String, Object>) currentWeather;
+
+        Map<String, Object> getTemperature = (Map<String, Object>) getWeather.get("current");
+
+        return Integer.parseInt(getTemperature.get("temperature").toString());
     }
 }
